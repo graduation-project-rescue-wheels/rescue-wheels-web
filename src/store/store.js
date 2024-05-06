02b7306593a87655c4cdbb55dc44b2dc0b3a9c5b
KhaledAlaa12 <khaledalaa1055@gmail.com>
2024-05-06 22:50:22 +0300

    handling serializable error, update reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,25 +1,41 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import AuthSlice from "./AuthSlice";
 import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist"
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import RepairCenterSlice from "./RepairCenterSlice";
+import EmergencyRequests from "./EmergencyRequests";
 
 const persistConfig = {
-    key: "root",
-    version: 1,
-    storage
-}
+  key: "root",
+  version: 1,
+  storage,
+};
 
-const reducer  = combineReducers({
-    AuthData :AuthSlice,
-    RepairCenterData :RepairCenterSlice
-
-})
+const reducer = combineReducers({
+  AuthData: AuthSlice,
+  RepairCenterData: RepairCenterSlice,
+  EmergencyRequest: EmergencyRequests,
+});
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const ConfigStore = configureStore({
-    reducer: persistedReducer,
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-export const persistor = persistStore(ConfigStore)
+export const persistor = persistStore(ConfigStore);
