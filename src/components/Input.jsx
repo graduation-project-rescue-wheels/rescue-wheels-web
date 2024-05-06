46e99f6f6bdb15180255dd11338a9c36bb385a5b
KhaledAlaa12 <khaledalaa1055@gmail.com>
2024-05-06 22:32:52 +0300

    added Input Component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,38 +1,34 @@
-import NearMeIcon from '@mui/icons-material/NearMe';
+const Input = ({
+  id,
+  name,
+  type,
+  onChange,
+  onBlur,
+  value,
+  formT,
+  formE,
+  text,
+}) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={type} className="form-label text-capitalize">
+        {text}
+      </label>
+      <div className="input position-relative">
+        <input
+          type={type}
+          className="form-control"
+          id={id}
+          name={name}
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value}
+        />
+      </div>
 
+      {formT && formE && <div className="alert alert-danger mt-2">{formE}</div>}
+    </div>
+  );
+};
 
-const Input = ({ id, name, type, onChange, onBlur, value, formT, formE, text }) => {
-    return (
-        <div className="mb-3">
-            <label htmlFor={type} className="form-label text-capitalize">{text}</label>
-            <div className="input position-relative">
-                {
-                    name === "location" && (
-                        <div
-                            className="icon"
-                            style={{ position: "absolute", cursor: "pointer", right: "2%", top: "8px" }}
-                        >
-                            <NearMeIcon />
-                        </div>
-
-                    )
-                }
-                <input
-                    type={type}
-                    className="form-control"
-                    id={id}
-                    name={name}
-                    onChange={onChange}
-                    onBlur={onBlur}
-                    value={value}
-                />
-            </div>
-
-            {formT && formE && (
-                <div className="alert alert-danger mt-2">{formE}</div>
-            )}
-        </div>
-    )
-}
-
-export default Input
+export default Input;
