84881f0b92deeadc2dd7fa7b6e632e7393ac73bf
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-05-07 00:38:10 +0300

    edit navbar make a loader in all screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,68 +1,66 @@
-import { useRef } from "react";
-import { Link, useNavigate } from "react-router-dom"
-import img from "../../assets/143086968_2856368904622192_1959732218791162458_n.png"
-import "./Navbar.css"
-import { useSelector } from "react-redux";
-
-
+import { NavLink } from "react-router-dom"
+import logo from '../../assets/logoo.png'
 const Navbar = () => {
-  const navigate = useNavigate()
-  const user = useSelector(state => state.AuthData.UserData);
-
-  let navRef = useRef();
-
-  const handlelogout = () => {
-    localStorage.removeItem('Token');
-    localStorage.removeItem('persist:root');
-    navigate("/login");
-  }
-
   return (
-    <nav className="navbar navbar-expand-lg fixed-top ps-5 pe-5" ref={navRef} style={{ backgroundColor: "#e48700" }}>
-      <div className="container-fluid">
-        <Link className="navbar-brand fw-bold" to="/HomePage" style={{ color: "white " }}>Rescue Wheels</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className="dropdown-center me-auto mb-2 mb-lg-0" style={{ zIndex: "2" }}>
-            <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "white " }}>
-              Join Us
-            </button>
-            <ul className="dropdown-menu" style={{ color: "white " }}>
-              <li><Link class="dropdown-item" to="/HomePage">as Technician</Link></li>
-              <li><Link class="dropdown-item" to="/HomePage">as Repair Center</Link></li>
-            </ul>
-          </div>
+    <div>
+  <div className=" bg-dark w-100 py-1">
+  <ul className="col-md-1 ms-auto justify-content-around mb-2 me-5 mb-lg-0 d-flex">
+  
+  
+  
+      <NavLink className="nav-link text-white" to="/about">About</NavLink>
+  
+  
+      <NavLink className="nav-link text-white" to="/services">Services</NavLink>
+  
+  </ul>
+</div>
 
-          <div className="dropdown-center" style={{ zIndex: "1" }}>
-            {
-              user?.length !== 0 ?
-                <>
-                  <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "white " }}>
-                    <img src={img} alt="" className="rounded-circle pic w-25" />
-                  </button>
-                  <ul className="dropdown-menu">
-                    <li><Link className="dropdown-item" to="/Settings">Settings</Link></li>
-                    <li><Link className="dropdown-item" to="/Vehicles">Vehicles</Link></li>
-                    <li><Link className="dropdown-item" onClick={handlelogout}>Logout</Link></li>
-                  </ul>
-                </>
-                :
-                <>
-                  <button 
-                    className="btn" 
-                    type="button" 
-                    onClick={()=>{navigate('/Login')}}
-                    style={{ color: "white " }}>
-                    Sign In
-                  </button>
-                </>
-            }
-          </div>
-        </div>
-      </div>
-    </nav>
+
+   <nav className="navbar navbar-expand-lg bg-light-subtle">
+  <div className="container-fluid">
+    <NavLink className="navbar-brand" href="#">
+      <img src={logo} alt="" style={{width:'50px', height:'50px'}}/>
+    </NavLink>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul className="navbar-nav mx-auto mb-2   mb-lg-0 ">
+        <li className="nav-item">
+          <NavLink className="nav-link " aria-current="page" to="/HomePage">Home</NavLink>
+        </li>
+        
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/">History</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/Emergency">Emergency</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/Vehicles">Vehicles</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/Settings">Settings</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/">Profile</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/Technician">Technician</NavLink>
+        </li>
+        
+      </ul>
+    {/* <form className="d-flex bg ms-auto rounded-pill bg-transparent" role="search">
+        <input className="form-control me-2 rounded-pill" type="search" placeholder="Search" aria-label="Search" />
+      </form> */}
+      
+     
+    </div>
+  </div>
+</nav>
+
+    </div>
   )
 }
 
