e72bb7e95f73f61947d612bda03c7da47fc78a5e
KhaledAlaa12 <khaledalaa1055@gmail.com>
2024-05-06 22:37:12 +0300

    added simple table component

---
+++ src/components/SimpleTable/SimpleTable.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+const SimpleTable = ({ title, rows, columns, styles }) => {
+  return (
+    <div className="p-3 bg-white table" style={styles}>
+      <h2 style={{ color: "var(--main-color)", fontWeight: "bold" }}>
+        {title}
+      </h2>
+      <DataGrid
+        style={{ border: "none" }}
+        autoHeight
+        rowHeight={90}
+        rows={rows.slice(-3).reverse()}
+        getRowId={(row) => row._id}
+        className="bg-white"
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        disableRowSelectionOnClick
+      />
+    </div>
+  );
+};
+
+export default SimpleTable;
