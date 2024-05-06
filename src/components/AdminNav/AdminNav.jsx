9c363ed380da1a7887c3c603321a152992352092
KhaledAlaa12 <khaledalaa1055@gmail.com>
2024-05-06 22:34:42 +0300

    add signedUser to Admin NavBar

--- src/components/AdminNav/AdminNav.jsx
+++ src/components/AdminNav/AdminNav.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
 import img from "../../assets/143086968_2856368904622192_1959732218791162458_n.png"
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import "./AdminNav.css"
 
 
 const AdminNav = ({name}) => {
+
+    const user = useSelector(state => state.AuthData.UserData);
+    const userName = `${user.firstName} ${user.lastName}`
+
     return (
-        <nav className="navbar navbar-expand-lg">
+        <nav className="navbar navbar-expand-lg w-100">
             <div className="container-fluid">
-                <Link className="navbar-text navbar-brand fw-bold position-relative" style={{ color: "black !important", fontSize: "1.7rem" }}>{name}</Link>
+                <Link className="navbar-text navbar-brand fw-bold position-relative" style={{ color: "black !important", fontSize: "clamp(1.3rem, 2.5vw, 1.7rem)" }}>{name}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div className="dropdown-center w-100 d-flex justify-content-end" style={{ zIndex: "1" }}>
-                        <button className="btn dropdown-toggle d-flex justify-content-end align-items-center column-gap-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <h6>User Name</h6>
-                            <img src={img} alt="" className="rounded-circle pic" style={{ width: "2rem" }} />
-                        </button>
-                        <ul className="dropdown-menu" style={{ left: "auto" }}>
-                            <li><Link className="dropdown-item" to="/">Settings</Link></li>
-                            <li><Link className="dropdown-item">Logout</Link></li>
-                        </ul>
-
+                    <div className="w-100 d-flex justify-content-end align-items-center gap-3" style={{ zIndex: "1" }}>
+                    <NotificationsIcon type="button" />
+                        <div className="d-flex align-items-center gap-3">
+                            <h6 className='mb-0'>{userName}</h6>
+                            {
+                                user.pic ? 
+                                <img src={user.pic} alt="" className="rounded-circle pic" style={{ width: "3rem" }} />
+                                :
+                                <img src={img} alt="" className="rounded-circle pic" style={{ width: "3rem" }} />
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
